fd007dc81d96a20838018504e77f66ee8ca848bb
哔哩哔哩 <fanzhizheng@bilibili.com>
2018-05-08 19:47:31 +0800

    init 项目

---
+++ src/components/GameScene.vue
@@ -0,0 +1,21 @@
+<template>
+    <g>dda</g>
+</template>
+<script>
+import BattleFieldScene from './BattleFieldScene'
+import StatisticsScene from './StatisticsScene'
+export default {
+    data() {
+        return {
+        }
+    },
+    beforeDestroy(){},
+    beforeMount(){},
+    updated(){
+
+    },
+}
+</script>
+<style lang="less" scoped>
+
+</style>
