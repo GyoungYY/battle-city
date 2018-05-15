67dba523b1e797f6111279b065f03a94c0c38d16
fanzhizheng <fanzhizheng@bilibili.com>
2018-05-15 15:46:41 +0800

    增加主状态

--- src/components/BattleFieldScene.vue
+++ src/components/BattleFieldScene.vue
@@ -3,42 +3,34 @@
         <hud></hud>
         <g className="battle-field" :transform="`translate(${B},${B})`">
             <rect :width="13 * B" :height="13 * B" fill="#000000" />
+            <steel-layer :steels="steels"></steel-layer>
         </g>
     </screen>
 </template>
 <script>
 import Hud from "../components/HUD.vue"
 import Screen from "../components/Screen.vue"
+import SteelLayer from './SteelLayer.vue'
 import { BLOCK_SIZE as B } from '../utils/constants'
 export default {
     data() {
         return {
             B,
             x: 0,
-            y: 0
+            y: 0,
+            steels:[]
         }
     },
     methods: {
-        onMouseDown() {
-            this.$emit();
-        },
-        onMouseLeave() {
-            this.$emit();
-        },
-        onMouseMove() {
-            this.$emit();
-        },
-        onMouseUp() {
-            this.$emit();
-        },
+
     },
     components: {
         Screen,
-        Hud
+        Hud,
+        SteelLayer
     }
 
 }
 </script>
 <style lang="less" scoped>
-
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -8,6 +8,11 @@ Vue.use(Vuex)
 const debug = process.env.NODE_ENV !== 'production'
 
 export default new Vuex.Store({
+  state: {
+    game: {
+      aaa: 1
+    }
+  },
   modules: {
     gameScene,
   },
